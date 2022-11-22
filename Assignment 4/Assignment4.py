0eb6180b01ed8e3838cb48473e9b41b0ef8e60c2
Brent <brenson00@gmail.com>
2022-11-22 15:28:33 -0800

    add new ui folder

--- Assignment 4/Assignment4.py
+++ Assignment 4/Assignment4.py
@@ -3,6 +3,19 @@ from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
 import pandas as pd
 
+class ComfirmDelete(QMainWindow):
+    def __init__(self):
+        super(ComfirmDelete,self).__init__()
+        uic.loadUi('comfirm_delete.ui', self)
+        #self.df_users = pd.read_excel('Assignment4.xlsx', sheet_name='users')
+        #self.user = self.df_users.loc[self.df_users.id == id].reset_index()
+        #self.id = id
+        #self.btn_ok.clicked.connect(self.ok)
+        #self.btn_cancel.clicked.connect(self.cancel)
+        print("dialogue opened")
+        self.show()
+
+
 class ShowUserGui(QMainWindow):
 
     def __init__(self, id):
@@ -10,13 +23,29 @@ class ShowUserGui(QMainWindow):
         uic.loadUi('show_user.ui', self)
         self.df_users = pd.read_excel('Assignment4.xlsx', sheet_name='users')
         self.user = self.df_users.loc[self.df_users.id == id].reset_index()
+        self.id = id
         self.le_username.setText(str(self.user.username[0]))
         self.le_password.setText(str(self.user.password[0]))
         self.lbl_photo.setPixmap(QPixmap(str(self.user.photo_path[0])))
         self.lbl_photo.setFixedWidth(300)
         self.lbl_photo.setFixedHeight(300)
+        self.btn_update.clicked.connect(self.update)
+        self.btn_delete.clicked.connect(self.delete)
         self.show()
 
+    def update(self):
+        new_user = self.le_username.text()
+        new_pass = self.le_password.text()
+        index = self.df_users[self.df_users['id'] == self.id].index.values
+        self.df_users.loc[index, ['username', 'password']] = [new_user, new_pass]
+        update_writer = pd.ExcelWriter('Assignment4.xlsx')
+        self.df_users.to_excel(update_writer, sheet_name = 'users', index = 0)
+        update_writer.save()
+        self.users_gui = UsersGui()
+        self.close()
+
+    def delete(self):
+        self.comfirm_delete = ComfirmDelete()
 
 class UsersGui(QMainWindow):
 
@@ -53,6 +82,7 @@ class UsersGui(QMainWindow):
     def show_user(self, id):
         self.show_user_gui = ShowUserGui(id)
         self.load_users_data()
+        self.close()
 
 
 app = QApplication([])
